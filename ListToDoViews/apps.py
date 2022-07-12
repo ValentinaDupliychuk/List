8ed3253c51e073d1ffc9f9d10e60193f508a757f
Valentina <valentinkka@yandex.ru>
2022-07-12 08:55:33 +0300

    tests

---
+++ ListToDoViews/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ListtodoviewsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ListToDoViews'
---
+++ common/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from django.views import View
+from django.http import HttpRequest, HttpResponse
+
+
+class Hello_World(View):
+    def get(self, request):
+        note = f"Hello world!"
+        return HttpResponse(note)
+
+
+class IndexView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        context = {"user": request.user.username, "version": "3.6"}
+        return render(request, "common/index.html", context)
